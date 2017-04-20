02b831b25f1480b6d377e14e612aef09f7990bbd
Guest <guest-m50rjw@dhcp141-150.ee.washington.edu>
2017-04-19 17:06:30 -0700

    finished parsing program for key inputs

--- toBits.c
+++ toBits.c
@@ -1,32 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 unsigned int_to_bin (unsigned k) {
 	return (k == 0 || k == 1 ? k : ((k % 2) + 10 * int_to_bin(k / 2)));
 }
 
-void get_bits(unsigned k) {
-	int bit;
-	do {
+int* toBits(char key) {
+
+        unsigned k;
+        int bit;
+        char exit_key = 'q';
+	static int a[8];	
+
+        k = int_to_bin((unsigned)key); // get key's ascii code
+
+       	//Parses the key's ascii code, diplaying one bit at a time
+	//while( k != NULL && key != exit_key) {
+	int i;
+	for(i = 0; i < 8; i++) {
 		bit = (k % 2);
-		k = k / 10;
-		printf("%d\n", bit);
-	} while(k != NULL);
+                k = k / 10;
+                printf("%d\n", bit);
+		a[i] = bit;
+	}
+
+	return a;
 }
 
-void char_to_bin() {
+int main() {
 	char key;
-	unsigned k;
-	char exit_key = 'q';
+	int *a;
+	scanf(" %c", &key);
+	
+	a = toBits(key);
 
-	do {
-        	scanf(" %c", &key);
-        	k = int_to_bin((unsigned)key);
-       		//printf("%d \n", k);
-		get_bits(k);
-	} while(int_to_bin((unsigned)key) != int_to_bin((unsigned)exit_key));
-}
+        int j;
+        for(j = 0; j < 8; j++) {
+                printf("%d", a[j]);
+        }
 
-int main(int argc, char **argv) {
-	char_to_bin();
 }

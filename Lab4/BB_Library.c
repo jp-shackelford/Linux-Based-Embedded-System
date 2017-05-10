54e7589df8cc7e1f5e93d905bd0045675898184f
Michael Walsh <michaelwalsh@D-69-91-165-165.dhcp4.washington.edu>
2017-05-09 18:23:57 -0700

    added base code for lab4

---
+++ Lab4/BB_Library.c
@@ -0,0 +1,69 @@
+#include <stdio.h>      // for File IO and printf
+#include <time.h>       // for usleep
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <sys/stat.h>
+#include <unistd.h>
+// file that when written to initalizes a GPIO folder 
+#define GPIO_EXPORT_PATH "/sys/class/gpio/export"
+#define PATH_BUF 1024
+ // Get Stream. Takes a file path, and mode type as parameters
+ // will wait for file to be initialized due to BB's timing delays
+ // Note: if the path passed in doesn't or will never exist, getStream
+ // will loop indefinetly
+FILE* getStream(char *path, char *mode) {
+    while(!fopen(path, mode)) {
+        usleep(1);
+    }
+    return fopen(path, mode);
+}
+
+  // combines fprintf and flush into one function
+  // call this to clean up the code a bit
+void writeToStream(FILE *stream, char *format, char *value) {
+    fprintf(stream, format, value);
+        fflush(stream);
+}
+
+// returns a pointer to value of a GPIO 
+FILE* initGPIO(int gpioNumber) {
+    FILE *exportP = getStream(GPIO_EXPORT_PATH, "direction");
+    writeToStream(exportP, "%d", gpioNumber);
+    FILE* currentP; 
+    char tempPath[PATH_BUF];
+    sprintf(tempPath, "%s%d%s", "/sys/class/gpio/gpio", gpioNumber, "/direction");
+    writeToStream(currentP, "%s", "out");
+    sprintf(tempPath, "%s%d%s", "/sys/class/gpio/gpio", gpioNumber, "/value");
+    return getStream(tempPath, "w"); 
+} 
+
+// returns a pointer to duty cycle of PWM
+// takes int as a parameter
+// 0 for EHRPWM1A, 1 for EHRPWM2A
+FILE* initPWM(int pwm) {
+	FILE* pwmSet = getStream("/sys/devices/bone_capemgr.9/slots", "w"); 
+	writeToStream(pwmSet, "%s", "am33xx_pwm");
+	writeToStream(pwmSet, "%s", "bone_pwm_P9_14");
+    writeToStream(pwmSet, "%s", "bome_pwm_p8_19"); 
+    char path[PATH_BUF];
+    // if path == 1, use EHRPWM2A
+    if(pwm) {
+        sprintf(path, "%s", "/sys/devices/ocp.3/pwm_test_P8_19.15/");
+    } else { // path == 0 use EHRPWM1A
+        sprintf(path, "%s", "/sys/devices/ocp.3/pwm_test_P9_14.15/"); 
+    }
+    char ppath[PATH_BUF];
+    char dpath[PATH_BUF];
+    char epath[PATH_BUF]; 
+    sprintf(ppath, "%s%s", path, "period");
+    sprintf(dpath, "%s%s", path, "duty");
+    sprintf(epath, "%s%s", path, "run");
+	FILE* period = getStream(ppath, "w");
+    writeToStream(period, "%d", 1020408); //Set Frequency to 980Hz
+	FILE* duty =  getStream(dpath, "w");
+	writeToStream(duty, "%d", 510204); // intialize to 50% duty cycle  
+	FILE* enable = getStream(epath, "w");
+	writeToStream(enable, "%d", 0); 
+    return duty; 
+}
---
+++ Lab4/MotorLibrary.c
@@ -0,0 +1,117 @@
+#include "BB_Library.c"
+// motor struct, contains GPIO information 
+// for a motor's controls 
+typedef struct {
+    FILE* IN1PIN;
+    FILE* IN2PIN;
+    FILE* PWM;
+    FILE* STDBY;
+    int OFFSET;
+} Motor; 
+
+// Returns a pointer to a Motor Struct
+// Will also initalize In1pin and In2pin to 0
+// initialize PWMpin to a frequency of 980hz with a duty cycle of 255
+// takes inputs In1, In2, PWM, STDBY, and offset to declare the struct as such
+Motor* motorinit(int In1pin, int In2pin, int PWMpin, int STBYpin, int offset)
+{
+  Motor *thism = malloc(sizeof(Motor));
+  thism->IN1PIN = initGPIO(In1pin);
+  thism->IN2PIN = initGPIO(In2pin);
+  thism->PWM  = initPWM(PWMpin);
+  thism->STDBY  = initGPIO(STBYpin);
+  thism->OFFSET = offset;
+  // TODO-> initialize these GPIO's 
+  return thism; 
+}
+
+// for testing...
+int main() {
+    initPWM(1); 
+}
+
+/*
+void Motor::drive(int speed)
+{
+  digitalWrite(Standby, HIGH);
+  speed = speed * Offset;
+  if (speed>=0) fwd(speed);
+  else rev(-speed);
+}
+void Motor::drive(int speed, int duration)
+{
+  drive(speed);
+  delay(duration);
+}
+
+void Motor::fwd(int speed)
+{
+   digitalWrite(In1, HIGH);
+   digitalWrite(In2, LOW);
+   analogWrite(PWM, speed);
+
+}
+
+void Motor::rev(int speed)
+{
+   digitalWrite(In1, LOW);
+   digitalWrite(In2, HIGH);
+   analogWrite(PWM, speed);
+}
+
+void Motor::brake()
+{
+   digitalWrite(In1, HIGH);
+   digitalWrite(In2, HIGH);
+   analogWrite(PWM,0);
+}
+
+void Motor::standby()
+{
+   digitalWrite(Standby, LOW);
+}
+
+void forward(Motor motor1, Motor motor2, int speed)
+{
+	motor1.drive(speed);
+	motor2.drive(speed);
+}
+void forward(Motor motor1, Motor motor2)
+{
+	motor1.drive(DEFAULTSPEED);
+	motor2.drive(DEFAULTSPEED);
+}
+
+
+void back(Motor motor1, Motor motor2, int speed)
+{
+	int temp = abs(speed);
+	motor1.drive(-temp);
+	motor2.drive(-temp);
+}
+void back(Motor motor1, Motor motor2)
+{
+	motor1.drive(-DEFAULTSPEED);
+	motor2.drive(-DEFAULTSPEED);
+}
+void left(Motor left, Motor right, int speed)
+{
+	int temp = abs(speed)/2;
+	left.drive(-temp);
+	right.drive(temp);
+	
+}
+
+
+void right(Motor left, Motor right, int speed)
+{
+	int temp = abs(speed)/2;
+	left.drive(temp);
+	right.drive(-temp);
+	
+}
+void brake(Motor motor1, Motor motor2)
+{
+	motor1.brake();
+	motor2.brake();
+} */ 

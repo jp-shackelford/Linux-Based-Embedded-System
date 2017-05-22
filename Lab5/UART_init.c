038fce6b1dc355257dc9ae4df2a90509a8f58bb2
alex <acf8@uw.edu>
2017-05-21 22:13:06 -0700

    Minor changes to UART stuff. Will try more tomorrow

--- Lab5/UART_init.c
+++ Lab5/UART_init.c
@@ -5,32 +5,30 @@
 #include <fcntl.h>
 #include <string.h>
 
+#include "BB_Library.c"
+
+
 int main() {
-	int fd = open("/dev/ttyO1", O_RDWR);
+	FILE* fd = getStream("/dev/ttyO1", O_RDWR);
 	char receive[100];
 	char buf[20];
 	size_t nbytes;
 	ssize_t bytes_written;
 	
-	if (fd < 0) {
-		printf("Failed to open UART\n");
-		exit(0);
-	}
-	
 	struct termios options;
 	tcgetattr(fd, &options);
 	
 	// Set up serial comm options
-	options.c_cflag = B115200 | CS8 | CREAD | CLOCAL; // 9600 Baud, 8 data bits
+	options.c_cflag = B9600 | CS8 | CREAD | CLOCAL; // 9600 Baud, 8 data bits
 	options.c_iflag = IGNPAR | ICRNL; // Ignore parity bits
 	tcflush(fd, TCIFLUSH);
 	tcsetattr(fd, TCSANOW, &options);
 	
-	strcpy(buf, "testing\n");
+	strcpy(buf, "$$$\n");
 	nbytes = strlen(buf);
 	
 	while (1) {
-		bytes_written = write(fd, buf, nbytes);
+		writeToStream(fd, buf, nbytes);
 		printf("%ld\n", bytes_written);
 		//read(fd, receive, sizeof(receive));
 		printf("%s\n", receive);

844eb25fd056ae4f20bcf37f343fa5eea1d97ee9
Guest <guest-hh3ja1@dhcp141-140.ee.washington.edu>
2017-05-23 15:31:56 -0700

    pretty BT reliable now

--- Lab5/UART_init.c
+++ Lab5/UART_init.c
@@ -5,10 +5,54 @@
 #include <fcntl.h>
 #include <string.h>
 
+#include "TankHeader.h"
+
+int shots = 5;
+
+int readButton(int press) {
+	if (press == 'q') { // if ESC
+		printf("%s\n", "quitting");
+		exit(EXIT_SUCCESS);
+		return QUIT;
+	} else if (press == 'w' || press == 'W') {
+		printf("%s\n", "UP");
+		return FORWARD;
+	} else if (press == 'a' || press == 'A') {
+		printf("%s\n", "LEFT");
+		return TURN_LEFT;
+	} else if (press == 's' || press == 'S') {
+		printf("%s\n", "DOWN");
+		return BACKWARD;
+	} else if (press == 'd' || press == 'D') {
+		printf("%s\n", "RIGHT");
+		return TURN_RIGHT;
+	} else if (press == 'f' || press == 'F') {
+		if (shots > 0) {
+			shots--;
+			printf("%s%d\n", "FIRE!            ", shots);
+			return FIRE;
+		} else {
+			printf("%s\n", "out of ammo! Must reload");
+			return NO_ACTION;
+		}
+	} else if (press == 'r' || press == 'R') {
+		if (shots < CAP) {
+			printf("%s\n", "Reloading");
+			sleep(5);
+			shots = 5;
+			printf("%s\n", "Done reloading");
+		}
+		return NO_ACTION;
+	} else {
+		return NO_ACTION;
+	}
+}
+
+
 int main() {
 	int fd = open("/dev/ttyO1", O_RDWR);
 	printf("%d\n", fd);
-	char receive[10];
+	char receive[2];
 	char buf[20];
 	size_t nbytes;
 	ssize_t bytes_written;
@@ -17,7 +61,7 @@ int main() {
 	tcgetattr(fd, &options);
 	
 	// Set up serial comm options
-	options.c_cflag = B9600 | CS8 | CREAD | CLOCAL; // 9600 Baud, 8 data bits
+	options.c_cflag = B115200 | CS8 | CREAD | CLOCAL; // 115200 Baud, 8 data bits
 	options.c_iflag = IGNPAR | ICRNL; // Ignore parity bits
 	tcflush(fd, TCIFLUSH);
 	tcsetattr(fd, TCSANOW, &options);
@@ -25,15 +69,15 @@ int main() {
 	strcpy(buf, "Hello there!\n");
 	write(fd, buf, strlen(buf));
 	
-	sleep(5);
+	sleep(2);
 	strcpy(buf, "Bye for now\n");
 	write(fd, buf, strlen(buf));
 	
 	printf("%s\n", "gonna try reading here...");
 	while (1) {
-		read(fd, receive, sizeof(receive));
+		tcflush(fd, TCIFLUSH);
+		read(fd, receive, 1); // Read only first char
 		printf("%s\n", receive);
-		sleep(1);
 	}
 }
 
